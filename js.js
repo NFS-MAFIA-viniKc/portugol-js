f52e0597be737b8c3ea17953c9bcbcb74fe1ab29
vinicius <vinicius_m_porto@estudante.sesisenai.org.br>
2025-07-16 08:05:20 -0300

    dados globais e funcao inicializacao

--- js.js
+++ js.js
@@ -25,3 +25,15 @@ class Passageiro {
       this.autorizada = false;
     }
   }
+
+  // Dados globais
+  const passageiros = [];
+  const bagagens = [];
+  const voos = [];
+  
+  // Inicialização dos voos
+  function inicializarVoos() {
+    voos.push(new Voo("AB123", "São Paulo", "08:30", 3));
+    voos.push(new Voo("CD456", "Rio de Janeiro", "10:15", 4));
+    voos.push(new Voo("EF789", "Salvador", "13:45", 5));
+  }
