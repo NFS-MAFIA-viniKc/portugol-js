2f5fef21a71a7317d95bcdd5e857be76a35612b0
vinicius <vinicius_m_porto@estudante.sesisenai.org.br>
2025-07-16 07:54:57 -0300

    estrutura de dados

---
+++ js.js
@@ -0,0 +1,11 @@
+    // Estrutura de dados
+class Passageiro {
+    constructor(nome, documento) {
+      this.nome = nome;
+      this.documento = documento;
+      this.statusEmbarque = false;
+      this.bagagemRegistrada = false;
+    }
+  }
+  
+  
